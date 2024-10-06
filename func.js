d26b6b3c5129cfd93393d935f31b22f5ad4b29dd
CianGrout <152493916+CianGrout@users.noreply.github.com>
2024-10-06 20:12:21 +0100

    Add files via upload

---
+++ func.js
@@ -0,0 +1,21 @@
+const names=["John","john doe", "Johnny"];
+const avaliable_names=["John","john doe", "Johnny"];
+
+
+function verify(username){
+    signin
+    if (username in names){
+
+    }
+}
+
+function pickname(username){
+    var nameselect=False
+    var name=""
+    while (nameselect==False){
+        name=names[Math.random()*(length(name)+1)];
+        if (name!=username){
+            nameselect=True;
+        }
+    }
+}
